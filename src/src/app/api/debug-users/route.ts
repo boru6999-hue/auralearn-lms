3a3d40211034d003b628917744e50cd76ef6013e
boru <boru6999@gmail.com>
2026-05-17 02:24:29 +0800

    remove duplicate src folder

--- src/src/app/api/debug-users/route.ts
+++
@@ -1,9 +0,0 @@
-import { connectDB } from "@/lib/db";
-import User from "@/models/User";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  await connectDB();
-  const users = await User.find({}).select("name email role createdAt").lean();
-  return NextResponse.json(users);
-}
